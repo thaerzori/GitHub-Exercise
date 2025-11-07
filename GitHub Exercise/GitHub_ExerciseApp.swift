24ab9e68ab48a154c08af670d26b9142d1c8aa53
Thaer Zori <thaer.zori@icloud.com>
2025-11-07 14:51:50 -0800

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Thaer Zori on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
